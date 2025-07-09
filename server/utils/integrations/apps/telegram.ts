5888a45da3b0822ee2c6d2a79edf6beada81cae1
amandesai01 <amandesai01@gmail.com>
2025-07-09 13:32:47 +0000

    feat: telegram integration

--- server/utils/integrations/apps/telegram.ts
+++ server/utils/integrations/apps/telegram.ts
@@ -1,6 +1,24 @@
-import { IntegrationApps } from '~~/shared/consts/integrations';
+import { FormValidationSchemas, IntegrationApps } from '~~/shared/consts/integrations';
 import { defineIntegration } from '../base';
 
 export default defineIntegration(IntegrationApps.Telegram, async (options) => {
-  console.log('Telegram integration handler called with options:', options);
+  const data = options.integrationConfig.data as Zod.infer<typeof FormValidationSchemas.Telegram>;
+  const { botToken, chatId } = data;
+
+  const url = `https://api.telegram.org/bot${botToken}/sendMessage`;
+
+  try {
+    await $fetch(url, {
+      method: 'POST',
+      body: {
+        chat_id: chatId,
+        text: 'message',
+      },
+    });
+  } catch (error: any) {
+    console.error('Failed to send Telegram message:', {
+      errorBody: error.data,
+    });
+    throw new Error('Telegram message sending failed');
+  }
 });
